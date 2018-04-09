6b9cc25ba7a8e90b6b6644715d1bb276a23e3325
conrad <conrad@arista.com>
2018-04-09 16:10:01 -0700

    Adding main scripts.

---
+++ logscript.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+########
+# Logwatcher with webhook function by Tyler Conrad, conrad@arista.com
+# based on work by Richard, https://eos.arista.com/syslog-triggered-event-scripts/
+# Usage -
+# event-handler LOGWATCHER
+#   trigger on-boot
+#   action bash ip netns exec ns-MGMT python /mnt/flash/logscript.py
+#   delay 60
+# Requirements:
+#  Needs DNS to resolve webhook destination endpoint
+#  Needs tcp reachability to destination endpoint IP
+#  Can be run from VRF or global table. example above uses VRF change to syntax below to run from global:
+#  action bash python /mnt/flash/logscript.py
+########
+
+import re, time, os, sys, Logging
+from os import stat
+from jsonrpclib import Server
+from os.path import abspath
+from stat import ST_SIZE
+from optparse import OptionParser
+import webhookfunction
+import socket
+
+#Enter your webhook URL here.
+webhook_url = ''
+username = socket.gethostname()
+text = ''
+
+# What log file to watch
+file = "/var/log/messages"
+
+# What expression in the log to match on - needs work.
+expression = '(.*RIB:.*|.*OSPF:.*|.*BGP:.*)'
+
+# Define Log
+Logging.logD( id="ANY_EVENT",
+              severity=Logging.logInfo,
+              format="%s",
+              explanation="Message to indicate that the any-event script has caught an event",
+              recommendedAction=Logging.NO_ACTION_REQUIRED
+)
+
+class LogTail:
+    def __init__(self, logfile, expression):
+        self.expression = expression
+        self.logfile = abspath(logfile)
+        self.f = open(self.logfile,"r")
+        file_len = stat(self.logfile)[ST_SIZE]
+        self.f.seek(file_len)
+        self.pos = self.f.tell()
+    def _reset(self):
+        self.f.close()
+        self.f = open(self.logfile, "r")
+        self.pos = self.f.tell()
+
+# Look for new entries in the log file
+
+    def tail(self):
+        while 1:
+            self.pos = self.f.tell()
+            line = self.f.readline()
+            if not line:
+                if stat(self.logfile)[ST_SIZE] < self.pos:
+                    self._reset()
+                else:
+                    time.sleep(1)
+                    self.f.seek(self.pos)
+            else:
+
+# Look for a matching line
+                if re.match(self.expression, line, re.M|re.I):
+                  text = line
+                  response = webhookfunction.webhook(webhook_url, username, text)
+
+# Run this thing
+tail = LogTail(file, expression)
+tail.tail()
---
+++ webhookfunction.py
@@ -0,0 +1,26 @@
+
+#######
+# webhook function, created by Tyler Conrad, conrad@arista.com
+# Based on https://gist.github.com/devStepsize/b1b795309a217d24566dcc0ad136f784
+#######
+import json
+import requests
+
+webhook_url = ''
+username = ''
+text = ''
+
+def webhook(webhook_url, username, text):
+  payload = {
+    'username': username,
+    'text': text
+  }
+  response = requests.post(
+      webhook_url, data=json.dumps(payload),
+      headers={'Content-Type': 'application/json'}
+  )
+  if response.status_code != 200:
+      raise ValueError(
+          'Request to slack returned an error %s, the response is:\n%s'
+          % (response.status_code, response.text)
+      )
